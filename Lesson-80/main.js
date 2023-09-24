61330acf1a43c2df4d9185d97be4d56fd8f963d3
Venger-P <141601373+Venger-P@users.noreply.github.com>
2023-09-24 18:35:22 +0700

    Add files via upload

---
+++ Lesson-80/main.js
@@ -0,0 +1,26 @@
+for (let i = 1; i <= 10; i = i + 2) {
+   console.log(`Пройден ${i} круг`)
+}
+
+
+for (let x = 2; x <= 12; x = x + 2) {
+   console.log(x)
+}
+
+
+for (let y = 10; y >= 1; y = y - 1) {
+   console.log(y)
+}
+
+
+let z = 0
+while (z < 12) {
+   z = z + 2
+   console.log(z)
+}
+
+let a = 11
+while (a > 1) {
+   a = a - 1
+   console.log(a)
+}
