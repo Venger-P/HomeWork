cc316252788d71de0ce3b98743df23fc93e6eb85
Venger-P <141601373+Venger-P@users.noreply.github.com>
2023-09-25 13:28:59 +0700

    Add files via upload

---
+++ Lesson-79/main.js
@@ -0,0 +1,27 @@
+const firstNumber = 5
+const secondNumber = 20
+
+let result = null
+
+if (firstNumber === 10 && secondNumber === 20) {
+    result = firstNumber + secondNumber
+    console.log(result)
+} else {
+    result =secondNumber - firstNumber 
+    console.log(result)
+}
+
+const alex = 'admin'
+
+alex === 'admin' ? console.log('Alex is admin') : console.log('Alex is not admin')
+
+
+const age = 18
+if (age >= 18) {
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вам еще нет 18')
+}
+
+const age2 = 18
+age2 >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
