58e2f5fa94ef45e91f39d1bda13d472c5c476a39
Venger-P <141601373+Venger-P@users.noreply.github.com>
2023-09-25 13:40:07 +0700

    Add files via upload

---
+++ Lesson-76/main.js
@@ -0,0 +1,14 @@
+const firstName = 'John'
+const lastName = 'Smith'
+let age = '20'
+
+console.log(firstName, lastName, age, 'y.o')
+
+
+const firstName2 = 'Pavel'
+const lastName2 = 'Zhuravlev'
+const age2 = '41'
+const growth = '172'
+const weight = '75'
+
+console.log(firstName2, lastName2, age2,'y.o,',growth,'cm,',weight,'kg.')
