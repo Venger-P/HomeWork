a7204216f1cccdae4d7d2d2ccc0115a4b0009a60
Venger-P <141601373+Venger-P@users.noreply.github.com>
2023-09-26 21:43:30 +0700

    Add files via upload

---
+++ Lesson-81 HomeWork/main.js
@@ -0,0 +1,13 @@
+const numbers2 = [13, 24, 31, 42, 57, 66, 78, 81, 94, 99]
+
+for (let x = 0; x < numbers2.length; x++) {
+      if ((numbers2[x] % 2) === 0)
+   console.log(numbers2[x])
+}
+
+
+const rainbow = ['red', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+for (let y = rainbow.length - 1; y >= 0; y--) {
+   console.log(rainbow[y])
+}
